39930459f6708b0ab8d1aa245a3ad9353683a648
Christian Ott II <cott2020@my.fit.edu>
2025-04-05 18:25:27 -0400

    Enhance NewTaskPage with ExtendedFloatingActionButton and preview setup

--- app/src/main/java/com/wordco/clockworkandroid/ui/pages/NewTaskPage.kt
+++ app/src/main/java/com/wordco/clockworkandroid/ui/pages/NewTaskPage.kt
@@ -2,17 +2,34 @@ package com.wordco.clockworkandroid.ui.pages
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import com.wordco.clockworkandroid.ui.elements.FloatingNavButton
+import androidx.navigation.compose.rememberNavController
+
+// TODO: Add buttons to bring the time and date pickers up
+// TODO: Implement dialogs for time and date pickers
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NewTaskPage(controller: NavHostController) = Scaffold(
-    floatingActionButton = { FloatingNavButton("Back", controller, "list") }
-) {
-    Text("PLACEHOLDER TEST", modifier = Modifier.fillMaxSize())
+    floatingActionButtonPosition = androidx.compose.material3.FabPosition.Center,
+    floatingActionButton = {
+        ExtendedFloatingActionButton(onClick = { /* TODO: Handle Add Task */ }) { Text("Add Task") }
+    },
+    modifier = Modifier.fillMaxSize(),
+    content = { Text("New Task Page") }
+)
+
+
+
+@Preview
+@Composable
+fun NewTaskPagePreview() {
+    val mockNavController = rememberNavController()
+    NewTaskPage(controller = mockNavController)
 }
