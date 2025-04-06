fb21fa0a15b612596d3981468db03fa2708057cb
Christian Ott II <cott2020@my.fit.edu>
2025-04-06 12:28:33 -0400

    Added InfiniteCircularList component for Android Compose. It provides a circular list implementation utilizing LazyColumn with snapping behavior for a seamless user experience.

---
+++ app/src/main/java/com/wordco/clockworkandroid/ui/elements/InfiniteCircularList.kt
@@ -0,0 +1,107 @@
+package com.wordco.clockworkandroid.ui.elements
+
+// import androidx.activity.ComponentActivity
+// import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+// import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+// import androidx.compose.foundation.layout.Row
+// import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+// import androidx.compose.ui.unit.dp
+// import androidx.compose.ui.unit.sp
+
+// AUTHOR: slaviboy (GitHub)
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun <T> InfiniteCircularList(
+    width: Dp,
+    itemHeight: Dp,
+    numberOfDisplayedItems: Int = 3,
+    items: List<T>,
+    initialItem: T,
+    itemScaleFact: Float = 1.5f,
+    textStyle: TextStyle,
+    textColor: Color,
+    selectedTextColor: Color,
+    onItemSelected: (index: Int, item: T) -> Unit = { _, _ -> }
+) {
+    val itemHalfHeight = LocalDensity.current.run { itemHeight.toPx() / 2f }
+    val scrollState = rememberLazyListState(0)
+    var lastSelectedIndex by remember {
+        mutableIntStateOf(0)
+    }
+    var itemsState by remember {
+        mutableStateOf(items)
+    }
+    LaunchedEffect(items) {
+        var targetIndex = items.indexOf(initialItem) - 1
+        targetIndex += ((Int.MAX_VALUE / 2) / items.size) * items.size
+        itemsState = items
+        lastSelectedIndex = targetIndex
+        scrollState.scrollToItem(targetIndex)
+    }
+    LazyColumn(
+        modifier = Modifier
+            .width(width)
+            .height(itemHeight * numberOfDisplayedItems),
+        state = scrollState,
+        flingBehavior = rememberSnapFlingBehavior(
+            lazyListState = scrollState
+        )
+    ) {
+        items(
+            count = Int.MAX_VALUE,
+            itemContent = { i ->
+                val item = itemsState[i % itemsState.size]
+                Box(
+                    modifier = Modifier
+                        .height(itemHeight)
+                        .fillMaxWidth()
+                        .onGloballyPositioned { coordinates ->
+                            val y = coordinates.positionInParent().y - itemHalfHeight
+                            val parentHalfHeight = (itemHalfHeight * numberOfDisplayedItems)
+                            val isSelected = (y > parentHalfHeight - itemHalfHeight && y < parentHalfHeight + itemHalfHeight)
+                            val index = i - 1
+                            if (isSelected && lastSelectedIndex != index) {
+                                onItemSelected(index % itemsState.size, item)
+                                lastSelectedIndex = index
+                            }
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = item.toString(),
+                        style = textStyle,
+                        color = if (lastSelectedIndex == i) {
+                            selectedTextColor
+                        } else {
+                            textColor
+                        },
+                        fontSize = if (lastSelectedIndex == i) {
+                            textStyle.fontSize * itemScaleFact
+                        } else {
+                            textStyle.fontSize
+                        }
+                    )
+                }
+            }
+        )
+    }
+}
