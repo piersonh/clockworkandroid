7e2acfc749f07d9e87f4f5b15b51f01409a6cc7e
P-Stelzer <pstelzer2023@my.fit.edu>
2025-09-15 22:26:44 -0400

    Added "You don't have any profiles page" for the profile list page

--- app/src/main/java/com/wordco/clockworkandroid/profile_list_feature/ui/ProfileListPage.kt
+++ app/src/main/java/com/wordco/clockworkandroid/profile_list_feature/ui/ProfileListPage.kt
@@ -1,10 +1,14 @@
 package com.wordco.clockworkandroid.profile_list_feature.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,20 +16,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.wordco.clockworkandroid.R
 import com.wordco.clockworkandroid.core.domain.util.DummyData
 import com.wordco.clockworkandroid.core.ui.composables.NavBar
 import com.wordco.clockworkandroid.core.ui.composables.PlusImage
@@ -95,18 +109,86 @@ private fun ProfileListPage (
             modifier = Modifier
                 .padding(paddingValues)
                 .background(color = MaterialTheme.colorScheme.primary)
-        )
-        {
+        ) {
             when (uiState) {
+                is ProfileListUiState.Retrieved if (uiState.profiles.isEmpty()) -> EmptyProfileList(
+                    onCreateProfileClick = onCreateNewProfileClick
+                )
                 is ProfileListUiState.Retrieved -> ProfileList(
                     uiState = uiState,
                     onProfileClick = onProfileClick
                 )
+
                 ProfileListUiState.Retrieving -> Text("Loading...")
             }
         }
     }
 }
+
+@Composable
+private fun EmptyProfileList(
+    onCreateProfileClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+    ) {
+        Spacer(modifier = Modifier.weight(0.04f))
+
+        Box (
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fanned_cards),
+                contentDescription = "Profiles",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.height(170.dp),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onPrimaryContainer)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                "You Don't Have Any Profiles!",
+                fontFamily = LATO,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 40.sp,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextButton(
+            onClick = onCreateProfileClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            ),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                //.height(50.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            Text(
+                text = "Create Profile",
+                fontFamily = LATO,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(0.15f))
+    }
+}
+
 
 @Composable
 private fun ProfileList(
@@ -145,6 +227,25 @@ private fun ProfileListPagePreview() {
         ProfileListPage(
             uiState = ProfileListUiState.Retrieved(
                 profiles = DummyData.PROFILES.map { it.toProfileListItem() }
+            ),
+            navBar = { NavBar(
+                items = FAKE_TOP_LEVEL_DESTINATIONS,
+                currentDestination = Unit::class,
+                navigateTo = {}
+            ) },
+            onProfileClick = { },
+            onCreateNewProfileClick = { },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EmptyProfileListPagePreview() {
+    ClockworkTheme {
+        ProfileListPage(
+            uiState = ProfileListUiState.Retrieved(
+                profiles = emptyList()
             ),
             navBar = { NavBar(
                 items = FAKE_TOP_LEVEL_DESTINATIONS,
